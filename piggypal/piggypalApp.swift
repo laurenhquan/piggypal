0fbeaabe2bdd169389a34d8fbc356b366fbd2556
laurenhquan <laurenhquan@gmail.com>
2025-11-06 20:11:07 -0800

    added base files

---
+++ piggypal/piggypalApp.swift
@@ -0,0 +1,17 @@
+//
+//  piggypalApp.swift
+//  piggypal
+//
+//  Created by csuftitan on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct piggypalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ piggypalTests/piggypalTests.swift
@@ -0,0 +1,17 @@
+//
+//  piggypalTests.swift
+//  piggypalTests
+//
+//  Created by csuftitan on 11/6/25.
+//
+
+import Testing
+@testable import piggypal
+
+struct piggypalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
