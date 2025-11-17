5c52cc5638301463694d9eda998d2f4acd9c978b
laurenhquan <laurenhquan@gmail.com>
2025-11-17 06:59:19 -0800

    created function GetBalance to calculate and return balance for given budget period

---
+++ piggypal/Functions/GetBalance.swift
@@ -0,0 +1,11 @@
+//
+//  GetBalance.swift
+//  piggypal
+//
+//  Created by csuftitan on 11/17/25.
+//
+
+import Foundation
+
+// input is payment period
+// output is ending/current balance for given payment period
