e2806366fac6aac6b5eef5883cdd4b8cf2bedc10
JKann2004 <112152074+JKann2004@users.noreply.github.com>
2025-11-21 13:16:25 -0800

    functionality works correctly, will later add alternating background

--- piggypal/Views/RecordView.swift
+++ piggypal/Views/RecordView.swift
@@ -8,11 +8,87 @@
 import SwiftUI
 
 struct RecordView: View {
+    @EnvironmentObject var controller: TransactionsController
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Use controller to access Transaction information from TransactionView
+        let transaction_data = controller.getAllTransactions()
+        
+        // Title
+        VStack(alignment: .center) {
+            Text("Transaction History")
+                .font(Font.largeTitle.bold())
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            
+            Divider()
+            
+            ScrollView {
+                Grid() {
+                    // Transaction History Labels
+                    GridRow {
+                        Text("Date")
+                        Text("Category")
+                        Text("Description")
+                        Text("Price")
+                    }
+                    
+                    .foregroundColor(Color("TextColor"))
+                    .font(.headline)
+                    Divider()
+                        .frame(height: 2)
+                        .overlay(Color.black)
+                    
+                    // Display No Recorded Transactions if there's no current transactions
+                    if transaction_data.count == 0 {
+                        Text("No Recorded Transactions")
+                            .foregroundColor(Color("TextColor"))
+                    }
+                    
+                    // Access information from each transaction
+                    ForEach(transaction_data, id: \.self) { data in
+                        let currencyCode = data.currencyUsed ?? ""
+                        let amount = data.amount?.doubleValue ?? 0
+                        let positive = abs(amount)
+                        let category = data.category ?? "N/A"
+                        let dateMade = data.dateMade ?? Date()
+                        let desc = data.desc ?? "N/A"
+                        
+                        // Display each entry
+                        GridRow {
+                            Text(dateMade.formatted(date: .numeric, time: .shortened))
+                                .multilineTextAlignment(.center)
+                            Text(category)
+                                .multilineTextAlignment(.center)
+                            Text(desc)
+                                .multilineTextAlignment(.center)
+                            Text(positive, format: .currency(code: currencyCode))
+                                .multilineTextAlignment(.trailing)
+                        }
+                        .foregroundColor(Color("TextColor"))
+                        Divider()
+                            .frame(height: 2)
+                            .overlay(Color.white)
+                    }
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color("CardColor"))
+                )
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
+// KEEP
+// Transaction History Icon DOUBLES when DELETED
+struct Info: Identifiable {
+    let id = UUID()
+}
+
+// Displays on Canvas
 #Preview {
-    RecordView()
+    RecordView().environmentObject(TransactionsController.shared)
 }
