2b43183afa40410300a8f65c7a9f036ff53a3ac9
laurenhquan <laurenhquan@gmail.com>
2025-11-28 15:31:13 -0800

    changed to use NavigationStack for consistency

--- piggypal/Views/TransactionView.swift
+++ piggypal/Views/TransactionView.swift
@@ -36,188 +36,175 @@ struct TransactionView: View {
 
     
     var body: some View {
-        VStack {
-            Form{
-                //Title
-                HStack {
-                    Text("Feed")
-                    Image(systemName: "dollarsign")
-                        .padding(8)
+        NavigationStack {
+            VStack {
+                Form {
+                    //Date
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Date")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        DatePicker(
+                            "Enter Date",
+                            selection: $selectedDate,
+                            displayedComponents: .date
+                        )
+                        .datePickerStyle(.compact)
+                        .padding()
                         .background(
-                            Circle()
-                                .fill(Color("Button2Color"))
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color("Button1Color"))
                         )
-                        .overlay(
-                            Circle()
-                                .stroke(Color.black, lineWidth: 3) // black border
-                        )
-                }
-                .font(.largeTitle.bold())
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-                
-                //Date
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Date")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    DatePicker(
-                        "Enter Date",
-                        selection: $selectedDate,
-                        displayedComponents: .date
-                    )
-                    .datePickerStyle(.compact)
+                    }
                     .padding()
                     .background(
                         RoundedRectangle(cornerRadius: 15)
-                            .fill(Color("Button1Color"))
+                            .fill(Color("CardColor"))
                     )
-                }
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color("CardColor"))
-                )
-                
-                //Amount
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Amount")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    HStack {
-                        TextField("Enter amount in ...", value: $amount, format: .currency(code: currencyCode))
-                            .keyboardType(.decimalPad)
-                            .textFieldStyle(PlainTextFieldStyle())
-                            .id(currencyCode)
-                        Picker("", selection: $currencyCode) {
-                            ForEach (codes, id: \.self) {c in
-                                Text(c)
-                                    .tag(c)
+                    
+                    //Amount TODO: take out currency option
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Amount")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            TextField("Enter amount in ...", value: $amount, format: .currency(code: currencyCode))
+                                .keyboardType(.decimalPad)
+                                .textFieldStyle(PlainTextFieldStyle())
+                                .id(currencyCode)
+                            Picker("", selection: $currencyCode) {
+                                ForEach (codes, id: \.self) {c in
+                                    Text(c)
+                                        .tag(c)
+                                }
                             }
                         }
-                    }
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color("Button1Color"))
-                    )
-                    
-                    Divider()
-                    
-                    Picker("Deposit or Withdrawal?", selection: $withdrawal) {
-                        Text("Withdrawal")
-                            .tag(true)
-                        Text("Deposit")
-                            .tag(false)
-                    }
-                }
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color("CardColor"))
-                )
-                
-                //Description
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Description")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    TextField("Enter description", text: $transactionTitle)
                         .padding()
-                        .textFieldStyle(PlainTextFieldStyle())
                         .background(
                             RoundedRectangle(cornerRadius: 10)
                                 .fill(Color("Button1Color"))
                         )
-                }
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color("CardColor"))
-                )
-                
-                //Category
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Category")
-                        .font(.headline)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Picker("Add to ...", selection: $category) {
-                        ForEach (categories, id: \.self) {c in
-                            Text(c)
-                                .tag(c)
+                        
+                        Divider()
+                        
+                        Picker("Deposit or Withdrawal?", selection: $withdrawal) {
+                            Text("Withdrawal")
+                                .tag(true)
+                            Text("Deposit")
+                                .tag(false)
                         }
                     }
                     .padding()
                     .background(
                         RoundedRectangle(cornerRadius: 15)
-                            .fill(Color("Button1Color"))
+                            .fill(Color("CardColor"))
+                    )
+                    
+                    //Description TODO: make description box bigger
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Description")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        TextField("Enter description", text: $transactionTitle)
+                            .padding()
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("Button1Color"))
+                            )
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color("CardColor"))
+                    )
+                    
+                    //Category
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Category")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Picker("Add to...", selection: $category) {
+                            ForEach (categories, id: \.self) {c in
+                                Text(c)
+                                    .tag(c)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color("Button1Color"))
+                        )
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color("CardColor"))
                     )
                 }
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 15)
-                        .fill(Color("CardColor"))
-                )
-            }
-            .scrollContentBackground(.hidden)
-            .background(Color.white)
-            
-            
-            HStack {
-                Button("Clear ", systemImage: "xmark") {
-                    //reset form
-                    selectedDate = Date()
-                    transactionTitle = ""
-                    category = ""
-                    amount = nil
-                    currencyCode = defaultCode
-                    withdrawal = true
-                }
-                .bold()
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color("Button2Color"))
-                )
+                .scrollContentBackground(.hidden)
+                .background(Color.white)
                 
-                Button("Submit", systemImage: "plus"){
-                    if amount == nil {
-                        notValid = true
-                    } else {
-                        //send transaction info to database
-                        var value: Decimal = amount ?? 0.0
-                        if withdrawal {
-                            value = value * -1
-                        }
-                        controller.feedPiggy(amount: value, currencyUsed: currencyCode, dateMade: selectedDate, category: category, desc: transactionTitle)
+                HStack {
+                    Spacer()
+                    
+                    Button("Clear", systemImage: "xmark") {
                         //reset form
                         selectedDate = Date()
                         transactionTitle = ""
-                        category = ""
+                        category = "Home & Utilities"
                         amount = nil
                         currencyCode = defaultCode
                         withdrawal = true
-                        //notification on submit ""
-                        isValid = true
                     }
-                }
-                .bold()
-                .foregroundColor(.black)
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color("Button2Color"))
-                )
-                .alert("Missing required field: Amount", isPresented: $notValid) {
-                    Button("OK", role: .cancel) {}
-                }
-                .alert("Done!", isPresented: $isValid) {
-                    Button("OK", role: .cancel) {}
+                    .bold()
+                    .foregroundColor(Color.red)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color("Button2Color"))
+                    )
+                    
+                    Button("Submit", systemImage: "plus"){
+                        if amount == nil {
+                            notValid = true
+                        } else {
+                            //send transaction info to database
+                            var value: Decimal = amount ?? 0.0
+                            if withdrawal {
+                                value = value * -1
+                            }
+                            controller.feedPiggy(amount: value, currencyUsed: currencyCode, dateMade: selectedDate, category: category, desc: transactionTitle)
+                            //reset form
+                            selectedDate = Date()
+                            transactionTitle = ""
+                            category = ""
+                            amount = nil
+                            currencyCode = defaultCode
+                            withdrawal = true
+                            //notification on submit ""
+                            isValid = true
+                        }
+                    }
+                    .bold()
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color("Button2Color"))
+                    )
+                    .alert("Missing required field: Amount", isPresented: $notValid) {
+                        Button("OK", role: .cancel) {}
+                    }
+                    .alert("Done!", isPresented: $isValid) {
+                        Button("OK", role: .cancel) {}
+                    }
+                    
+                    Spacer()
                 }
             }
-            
-            Spacer()
+            .navigationTitle("Feed")
         }
         .background(.white)
     }
